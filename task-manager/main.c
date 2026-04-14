9ea0aba2142295f78443e2bc0f9c24b23106df77
Ali Zahid Alavi <aalavi2401@conestogac.on.ca>
2026-04-14 09:50:13 -0400

    added menu and user choice code

--- task-manager/main.c
+++ task-manager/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>
+void addTask();
 
 int main() {
 
+	printf("----------WELCOME----------\n");
+
+	bool running = true;
+	int userChoice;
+	while (running) {
+		printf("1. Add a task\n2. Remove a task\n3. Update a task\n4. Display all tasks\n5. Mark as completed\n6. Filter tasks\n9 to QUIT PROGRAM\n");;
+		printf("Enter your choice: ");
+		scanf_s("%d", &userChoice);
+
+		if (userChoice == 9) {
+			running = false; 
+			printf("Exiting program!");
+		}
+	}
 
 	return 0;
 }
